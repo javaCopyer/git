7ec9c9891b835c638bbd6154b01b2b45f6bf6c47
sky <zxc@domain123.com>
2019-04-16 23:02:59 +0800

    haha

--- src/git/Test.java
+++ src/git/Test.java
@@ -1,21 +1,5 @@
 package git;
 
 public class Test {
-<<<<<<< HEAD
-123yuyuy
-=======
-<<<<<<< feature
-<<<<<<< master
-<<<<<<< master
-1123
-=======
-123
->>>>>>> 87373c9 123
-=======
-1123
->>>>>>> 93625fa 123333
-=======
-123333
->>>>>>> 1a0c2e8 23
->>>>>>> branch 'master' of https://github.com/javaCopyer/git.git
+aha
 }
